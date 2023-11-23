f90511d4ed08763fb8054400946685daf8cbb7e7
ranu <ranushka543@gmail.com>
2023-11-23 11:46:59 +0400

    bug fix

--- app/checklist/GoogleSheetForm.tsx
+++ app/checklist/GoogleSheetForm.tsx
@@ -4,7 +4,7 @@ import FormGenerator from './FormGenerator'
 import formatGoogleSheetData from '@/helpers/formatGoogleSheetData'
 
 const CSV_FILE_URL =
-  'https://docs.google.com/spreadsheets/d/e/2PACX-1vQrIfiKbE1n08igSN2uMNJBbfe4_N_mKZSsy5t4NrWtjW7GwKAOJNXlAsUp6I9s4zQXQBbbYPF9Wn_8/pub?output=csv&v1'
+  'https://docs.google.com/spreadsheets/d/e/2PACX-1vQrIfiKbE1n08igSN2uMNJBbfe4_N_mKZSsy5t4NrWtjW7GwKAOJNXlAsUp6I9s4zQXQBbbYPF9Wn_8/pub?output=csv&v2'
 
 async function getData() {
   const response = await fetch(CSV_FILE_URL)
